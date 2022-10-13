73347734fc77d719c7b00cb201c220c5230f4d44
Xterlo <xterlo@yandex.ru>
2022-10-13 17:48:49 +0300

    +tg db class

---
+++ Core/TgSync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TgSF.Core
+{
+    public class TgSync
+    {
+        public int MessageID;
+        public string Caption;
+        public string FileName;
+        public DateTime SendTime;
+
+        public TgSync(Telegram.Bot.Types.Message msg)
+        {
+            MessageID = msg.MessageId;
+            Caption = msg.Caption;
+            FileName = msg.Document.FileName;
+            SendTime = msg.Date;
+        }
+    }
+}
