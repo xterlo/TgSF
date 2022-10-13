21e60e8b67b2b98930ff3fa10c8839983bbc0c52
Xterlo <xterlo@yandex.ru>
2022-10-13 17:50:43 +0300

    *add edit message by ID (normally not implemented)

--- Core/TgFunctions.cs
+++ Core/TgFunctions.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.InputFiles;
 
 namespace TgSF.Core
@@ -57,22 +58,35 @@ namespace TgSF.Core
 
         public class TgChat : TgFunctions
         {
-            public static async Task<int> SendFile(string filePath)
+            public static async Task<Telegram.Bot.Types.Message> SendFile(FilesSync file)
             {
-                
-                var fileStream = new FileStream(filePath,FileMode.Open);
-                var fileToSend = new InputOnlineFile(fileStream);
+                var fileStream = new FileStream(file.FilePath,FileMode.Open);
+                var fileToSend = new InputOnlineFile(fileStream,file.FileName);
                 try
                 {
-                    var a = await Settings.TGBot.SendDocumentAsync(Settings.ChatId, fileToSend, caption: filePath);
-                    return a.MessageId;
+                    var a = await Settings.TGBot.SendDocumentAsync(Settings.ChatId, fileToSend, caption: file.FilePath);
+                    return a;
                 }
                 catch (Exception e)
                 {
                     MessageBox.Show(e.Message);
-                    return -1;
+                    return null;
                 }
+            }
 
+            public static async Task<bool> EditMessage(FilesSync file)
+            {
+                var fileStream = new FileStream(file.FilePath, FileMode.Open);
+                var b = new InputMediaDocument(new InputMedia(fileStream, file.FileName));
+                try
+                {
+                    await Settings.TGBot.EditMessageMediaAsync(file.TGMessageID.ToString(), b);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
             }
         }
     }
